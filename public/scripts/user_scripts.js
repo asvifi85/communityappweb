51076cca9657ed886fdb6789de45901e8f9fd090
vinaywebseo <vinaywebseo@gmail.com>
2016-05-31 07:16:34 +0530

    table view of users

--- public/scripts/user_scripts.js
+++ public/scripts/user_scripts.js
@@ -1,4 +1,18 @@
  firebase.database().ref('/users').on('value', function(snapshot) {
                  console.log( snapshot.val());
-                 $("#usersList").html(JSON.stringify(snapshot.val()));
+                 var usersData = snapshot.val();
+                 var table = [];
+                 var tableRow = "";
+                 for(var i in usersData){
+                        console.log(i);
+                        tableRow += "<tr><td><input type='checkbox' id='"+i+"' /></td>";
+                        tableRow += "<td>"+usersData[i].name+"</td>";
+                        tableRow += "<td>"+usersData[i].email+"</td>";
+                         tableRow += "<td>"+usersData[i].phone+"</td></tr>";
+                      table.push(tableRow);
+                     
+                 }
+                 var tablerows = table.join();
+                 
+                 $("#usersList table tbody").html(tablerows);
 });
