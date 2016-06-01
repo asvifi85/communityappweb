9cb7d47e9b84aee68b7abab266aa9c3ced46d198
asvifi85 <asvifi85@gmail.com>
2016-06-01 05:33:39 +0530

    push server

--- public/scripts/user_scripts.js
+++ public/scripts/user_scripts.js
@@ -1,3 +1,11 @@
+    // Initialize Firebase
+   var config = {
+    apiKey: "AIzaSyAj4jnNhVMNREWAwrXb_rDmXEBrxxeRoqI",
+    authDomain: "community-app.firebaseapp.com",
+    databaseURL: "https://community-app.firebaseio.com",
+    storageBucket: "firebase-community-app.appspot.com",
+  };
+   var fbref = firebase.initializeApp(config);
  firebase.database().ref('/users').on('value', function(snapshot) {
                  console.log( snapshot.val());
                  var usersData = snapshot.val();
@@ -16,4 +24,19 @@
                  var tablerows = table.join();
                  
                  $("#usersList table tbody").html(tablerows);
+});
+$("#submitNotice").click(function(e){
+		e.preventDefault();
+		
+		var notice = $("#noticebox").val();
+		var messageListRef =  firebase.database().ref('/nofications');
+		console.log(messageListRef)
+    messageListRef.push({ 'posted-date':Date(), 'text':notice }, function(error) {
+  if (error) {
+    alert("Data could not be saved." + error);
+  } else {
+    alert("Data saved successfully.");
+	$("#postnotice").submit();
+  }
+});
 });
--- routes/users.js
+++ routes/users.js
@@ -6,5 +6,11 @@ router.get('/', function(req, res, next) {
  // res.send('respond with a resource');
    res.render('users', { title: 'Community Web App (admin for mobile)' });
 });
+/* GET users listing. */
+router.post('/submitNotice', function(req, res, next) {
+  res.send('form submitted');
+  console.log(req.body);
+  // res.render('users', { title: 'Community Web App (admin for mobile)' });
+});
 
 module.exports = router;
