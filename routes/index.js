2d98d466984986c26bdc515b5ecd06fdb31fe392
asvifi85 <asvifi85@gmail.com>
2016-07-21 16:59:24 +0530

    implemented post service

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,11 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Community Web App (admin for mobile)' });
 });
-
+router.post("/postcall", function(req, res) {
+    if(!Object.keys(req.body).length) {
+        return res.send({"status": "error", "message": "missing a parameter"});
+    } else {
+        return res.send({"status": "success"});
+    }
+});
 module.exports = router;
